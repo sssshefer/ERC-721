cd63aa31770cab123ddef51c4825db5c81575519
Shefer Arkadii <arkadiy.shefer@gmail.com>
2024-07-24 11:03:50 +0300

    IERC721Metadata and IERC721 interfaces

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -1,6 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ERC721 {
-    
+import "./IERC721Metadata.sol";
+
+contract ERC721 is IERC721Metadata{
+    string public name;
+    string public symbol;
+
+    constructor (string memory _name, string memory _symbol){
+        name = _name;
+        symbol = _symbol;
+    }
 }
